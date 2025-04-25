d238d2ad69ef070c0aafd8676c60f2743d741628
OuterCyrex <a15839962056@outlook.com>
2025-04-25 20:22:40 +0800

    init

---
+++ src/pages/about/about.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		{{ a }}
+	</view>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const a = ref<number>(1)
+
+    return {
+      a
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
